95c382891036497f1c94a0192c309eea2fbd20f5
DSoftTech <81322397+DSoftTech@users.noreply.github.com>
2021-08-08 02:44:03 +0100

    moved the repository class to another directory, wrote logic into the repository

---
+++ RoomDB/app/src/main/java/com/example/roomdatabase/data/Repository.kt
@@ -0,0 +1,12 @@
+package com.example.roomdatabase.data
+
+import androidx.lifecycle.LiveData
+
+class Repository(private val localDbDao: DbDao) {
+
+    val allUsersInDB: LiveData<List<User>> = localDbDao.getAllUsers()
+
+    suspend fun insertUser(user: User) {
+        localDbDao.insetUser(user)
+    }
+}
